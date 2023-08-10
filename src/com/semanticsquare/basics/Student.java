18cd479846847881a2481892a0f15e909a20fe6f
Cheikh GAYE <gayec270198@gmail.com>
2023-08-10 14:05:24 +0000

    Integers type

--- src/com/semanticsquare/basics/Student.java
+++ src/com/semanticsquare/basics/Student.java
@@ -3,10 +3,13 @@ class Student {
 	byte age = 18;
     short rank = 165; //[-128, 127]
     long phone = 22_345_678_90L; //Java -- readability
-
+    
     // Integer literals: int literal, long literal
     int maxValue = Integer.MAX_VALUE; 
     int minValue = Integer.MIN_VALUE;
+
+    byte maxValueB = Byte.MAX_VALUE; 
+    byte minValueB = Byte.MIN_VALUE; 
 
 	void compute() {	    
 		int nextId = id + 1;
@@ -17,6 +20,8 @@ class Student {
 		System.out.println("phone: " + phone);
 		System.out.println("max int: " + maxValue);
 		System.out.println("min int: " + minValue);
+		System.out.println("max byte: " + maxValueB);
+		System.out.println("min byte: " + minValueB);
 	}
 	
 	public static void main(String[] args) {

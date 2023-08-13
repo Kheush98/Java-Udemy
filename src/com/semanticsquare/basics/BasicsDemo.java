2557ceb7f23d9808e6300d3dc4eed399689c32d2
Cheikh GAYE <gayec270198@gmail.com>
2023-08-13 13:29:29 +0000

    learning Arrays

--- src/com/semanticsquare/basics/BasicsDemo.java
+++ src/com/semanticsquare/basics/BasicsDemo.java
@@ -63,10 +63,47 @@ static void primitives() {
   System.out.println("cChar: " + cChar);
   }
 
+  static void arrays() {
+    int[] scores = new int[4];
+    scores[0] = 90;
+    scores[1] = 70;
+    scores[2] = 80;
+    scores[3] = 100;
+
+    //other way to declare arrays variables
+    /*
+      int[] scores = new int[] {90, 70, 80, 100};
+      int[] scores = {90, 70, 80, 100};   
+    */
+
+    System.out.println("\nInside arrays...");
+    System.out.println("Mid-Term 1: " + scores[0]);
+    System.out.println("Mid-Term 2: " + scores[1]);
+    System.out.println("Final: " + scores[2]);
+    System.out.println("Project: " + scores[3]);
+    System.out.println("Lenght: " + scores.length);
+
+    Student[] students = new Student[3];
+    students[0] = new Student();
+    students[1] = new Student();
+    students[2] = new Student();
+    //other way to declare arrays variables (object)
+    /* 
+      Student[] students = new Student[3] {new Student(), new Student(), new Student()}; 
+      Student[] students = {new Student(), new Student(), new Student()}; 
+    */
+    students[0].name = "Cheikh";
+    students[1].name = "Fatima";
+    students[2].name = "Kheush";
+    System.out.println("Student 1: " + students[0].name);
+    System.out.println("Student 2: " + students[1].name);
+    System.out.println("Student 2: " + students[2].name);
+  }
 public static void main(String[] args) {	
   // Language Basics 1
   //print();				
   //primitives();
-  typeCasting(); 
+  // typeCasting(); 
+  arrays();
   }       
 }

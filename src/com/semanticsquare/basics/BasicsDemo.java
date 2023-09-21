ab5b44983f35f44f207d7e3cf1d3f3b0c5508444
Cheikh GAYE <gayec270198@gmail.com>
2023-09-21 15:41:23 +0000

    Operators

--- src/com/semanticsquare/basics/BasicsDemo.java
+++ src/com/semanticsquare/basics/BasicsDemo.java
@@ -168,15 +168,29 @@ static void primitives() {
 	  System.out.println("Lenght : " + list.length);
   }
 
+  static void charTypePromotion() {
+	  System.out.println("\nInside charTypePromotion ...");
+	  char char1 = 50; // Will be assigned corresponding UTF16 value 2
+	  System.out.println("char1: " + char1); 
+	  System.out.println("(73 - char1): " + (73 - char1)); // char1 gets promoted to int, i.e., decimal equivalent 50 in UTF16 is used	  
+	  System.out.println("(char1 - '3'): " + (char1 - '3')); // char1 & '3' are promoted to ints	
+	  System.out.println("('a' + 'b'): " + ('a' +'b')); // 'a' & 'b' are promoted to ints and the respective equivalents 97 & 98 are added
+  }
+		
   public static void main(String[] args) {	
-  // Language Basics 1
-  //print();				
-  //primitives();
-  // typeCasting(); 
-  // arrays();
-  // threeDimensionalArrays();
-  varargsOverload(true, 1, 2, 3);
-  varargsOverload(true, 1, 2, 3, 4, 5, 6, 7, 8);
-  varargsOverload(true);
+	  // Language Basics 1
+	  //print();				
+	  //primitives();
+	  //typeCasting();
+	  //arrays();	
+      //threeDimensionalArrays();	
+      /*varargsOverload(true, 1, 2, 3);
+      varargsOverload(true, 1, 2, 3, 4, 5, 6, 7, 8);	  
+      varargsOverload(true);*/
+	  
+	  //preAndPost();
+	  //compoundArithmeticAssignment();
+	  //isOddOrEven(51);
+	  charTypePromotion();
   }       
 }

16d40b860812c9df0ef671d07e7c77d0456861f5
Cheikh GAYE <gayec270198@gmail.com>
2023-10-25 16:07:10 +0000

    Type string

--- src/com/semanticsquare/basics/BasicsDemo.java
+++ src/com/semanticsquare/basics/BasicsDemo.java
@@ -1,9 +1,6 @@
-package com.semanticsquare.basics;
-
-import java.util.Date;
-import java.util.ArrayList;
-
-class BasicsDemo {
+  //package com.semanticsquare.basics;
+  
+  class BasicsDemo {
     // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
     static void print() {
       System.out.println("\n\nInside print ...");
@@ -175,6 +172,73 @@ class BasicsDemo {
 		
 	    System.out.println("num: " + num); // prints 55		
     }
+	
+	static void labeledContinue() {
+	    System.out.println("\nInside labeledContinue ...");
+	    int num = 0;
+		
+	    outermost: for (int i = 0; i < 10; i++) {
+	        for (int j = 0; j < 10; j++) {    
+	            if (i == 5 && j == 5) {
+	                continue outermost;
+	            }
+	            num++;
+	        }
+	    }
+		
+	    System.out.println("num: " + num); // prints 55		
+    }
+	
+	static void stringExamples() {
+		System.out.println("\nInside stringExamples ...");
+		String s = "hello world!";
+		System.out.println("s: " + s);
+		
+		System.out.println("\ns.length(): " + s.length());
+		System.out.println("s.isEmpty(): " + s.isEmpty());
+		
+		// Comparison
+		System.out.println("\ns.equals(\"HELLO WORLD!\"): " + s.equals("HELLO WORLD!"));
+		System.out.println("s.equalsIgnoreCase(\"HELLO WORLD!\"): " + s.equalsIgnoreCase("HELLO WORLD!"));
+		System.out.println("s.compareTo(\"HELLO WORLD!\"): " + s.compareTo("HELLO WORLD!"));
+		
+		// Searching
+		System.out.println("\ns.contains(\"HELLO WORLD!\"): " + s.contains("HELLO WORLD!"));
+		System.out.println("s.contains(\"world!\"): " + s.contains("world!"));
+		System.out.println("s.startsWith(\"HELLO WORLD!\"): " + s.startsWith("HELLO WORLD!"));
+		System.out.println("s.startsWith(\"hello world!\"): " + s.startsWith("hello world!"));
+		System.out.println("s.endsWith(\"!\"): " + s.endsWith("!"));
+		System.out.println("s.indexOf(\"lo\"): " + s.indexOf("lo"));
+		System.out.println("s.indexOf(\"o\"): " + s.indexOf('o'));
+		System.out.println("s.lastIndexOf(\"o\"): " + s.lastIndexOf('o'));
+		
+		// Examining individual characters
+		System.out.println("\ns.charAt(4): " + s.charAt(4));
+		
+		// Extracting substrings
+		System.out.println("\ns.substring(4): " + s.substring(4));
+		System.out.println("s.substring(4, 9): " + s.substring(4, 9));
+		
+		// Case conversions (Note: String is immutable. So, only a copy is returned)
+		System.out.println("\ns.toUpperCase(): " + s.toUpperCase());
+		System.out.println("s.toLowerCase(): " + s.toLowerCase());
+		
+		System.out.println("\ns.trim(): " + s.trim()); // returns a copy of string after trimming any leading & trailing white spaces
+		
+		
+		// Replace (e.g., replace comma's with white-space)
+		System.out.println("\ns.replaceAll(\"o\", \"r\"): " + s.replaceAll("o", "r"));
+		
+		// Split (e.g., split a document into words or split a line of text by tab or comma or white space)
+		System.out.println("\ns.split(\"o\"): ");
+		String[] sa = s.split("o");
+		for (String temp : sa) {
+			System.out.println(temp);
+		}
+		
+		// Static method (includes overloaded methods)
+		System.out.println("\nString.valueOf(1.3): " + String.valueOf(1.3));
+	}
 		
 	public static void main(String[] args) {	
 	  // Language Basics 1
@@ -190,8 +254,9 @@ class BasicsDemo {
 	  //bitwiseOperators();
 	  //switchExample();
 	  //labeledBreak();
-	  Date date;
-	  ArrayList list;
-	  java.sql.Date date2;
+	  //labeledContinue();
+      // import demo
+      // package creation demo
+      stringExamples();	  
     }       
   }

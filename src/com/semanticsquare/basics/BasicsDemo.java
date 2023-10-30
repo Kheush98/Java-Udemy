f0eb5821db3da1913e2a67902618635c1634fc92
Cheikh GAYE <gayec270198@gmail.com>
2023-10-30 14:38:59 +0000

    StringBuffer

--- src/com/semanticsquare/basics/BasicsDemo.java
+++ src/com/semanticsquare/basics/BasicsDemo.java
@@ -272,6 +272,20 @@
       // import demo
       // package creation demo
       //stringExamples();	
-      stringPool();	  
+      //stringPool();	 
+	  
+	  String s = "Hello " + " world!";
+	  System.out.println("s: " + s);
+
+	  StringBuffer sb = new StringBuffer(s);
+	  sb.append(" good").append(" morning :)");
+	  System.out.println("sb: " + sb.toString());
+	  System.out.println("sb.length: " + sb.length());
+	  sb.delete(1, 5);
+	  System.out.println("sb: " + sb.toString());
+	  System.out.println("sb.length: " + sb.length());
+	  sb.insert(1, "ey");
+	  System.out.println("sb: " + sb.toString());
+	  System.out.println("sb.length: " + sb.length());
     }       
   }

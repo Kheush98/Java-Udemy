ec8e75596e1b15f573ac59365982a39958c8d0e8
Cheikh GAYE <gayec270198@gmail.com>
2024-01-04 10:15:53 +0000

    Math class

--- demo/src/com/semanticsquare/basics/BasicsDemo.java
+++ demo/src/com/semanticsquare/basics/BasicsDemo.java
@@ -1,7 +1,8 @@
 package com.semanticsquare.basics;
-  
-  class BasicsDemo {
-    // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
+
+class BasicsDemo {
+		
+	// Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
     static void print() {
       System.out.println("\n\nInside print ...");
       System.out.println("Hello, world!!"); // Advance cursor to beginning of next line
@@ -11,7 +12,7 @@ package com.semanticsquare.basics;
       System.out.print(" ");                // Print a space
       System.out.print("world!!");
     }
-	
+    	
 	static void primitives() {
 	  System.out.println("\n\nInside primitives ..."); 
 	  // literals demo
@@ -30,12 +31,30 @@ package com.semanticsquare.basics;
 	  System.out.println("charInt: " + charInt);
 	  char charUnicode1 = '\u0041'; // 4 * (16 power 1 )+ 1 * (16 power 0)
 	  System.out.println("charUnicode1: " + charUnicode1);
-	  char charUnicode2 = 0x41;     // 4 * (16 power 1 )+ 1 * (16 power 0)
-	  System.out.println("charUnicode2: " + charUnicode2);
+	  char charHex = 0x41;     // 4 * (16 power 1 )+ 1 * (16 power 0)
+	  System.out.println("charHex: " + charHex);
 	  char charBinary = 0b01000001; // binary corresponding to decimal 65
-	  System.out.println("charBinary: " + charBinary);	  		  	  
+	  System.out.println("charBinary: " + charBinary);
     }
-  
+	
+	public static void booleanDemo() {
+		  System.out.println("\n\nInside booleanDemo ..."); 
+		  boolean isInsured = true;
+		  double coverageByInsurance = 0.8;
+		  double billAmount = 1000.0;
+		  
+		  double amountPaidByInsurance = 0.0;
+		  double amountPaidByPatient = billAmount;
+		  
+		  if (isInsured) {
+			amountPaidByInsurance = billAmount * coverageByInsurance;
+			amountPaidByPatient = billAmount - amountPaidByInsurance;
+		  }
+		  
+		  System.out.println(amountPaidByInsurance);
+		  System.out.println(amountPaidByPatient);
+	}
+		
     static void typeCasting() {
       System.out.println("\nInside typeCasting ...");
       // Explicit casting
@@ -66,14 +85,41 @@ package com.semanticsquare.basics;
     }
 	
 	static void arrays() {
-	   System.out.println("\nInside arrays ...");
-	   int[] myArray = new int[]{9, 11, 2, 5, 4, 4, 6};
-       System.out.println("myArray.length: " + myArray.length);
-	   System.out.println("myArray[1]: " + myArray[1]);
-	   System.out.println("myArray[7]: " + myArray[7]);	   
+	      System.out.println("\nInside arrays ...");
+		  int[] scores = new int[4];	
+
+		  scores[0] = 90;
+		  scores[1] = 70;
+		  scores[2] = 80;
+		  scores[3] = 100;
+		  
+		  /*int[] scores = new int[] {90, 70, 80, 100};*/
+		  
+		  //int[] scores = {90, 70, 80, 100};
+		  
+		  System.out.println("Mid-Term 1: " + scores[0]);
+		  System.out.println("Mid-Term 2: " + scores[1]);
+		  System.out.println("Final: " + scores[2]);
+		  System.out.println("Project: " + scores[3]);
+		  System.out.println("# exams: " + scores.length);
+		  
+		  Student[] students = new Student[3];//{new Student(), new Student(), new Student()};
+		  students[0] = new Student();
+		  students[1] = new Student();
+		 // students[2] = new Student();
+		  /*students[0].name = "John";
+		  students[1].name = "Raj";
+		  students[2].name = "Anita";*/
+		  System.out.println("Student 1: " + students[0]);
+		  System.out.println("Student 2: " + students[1]);
+		  System.out.println("Student 3: " + students[2]);
+	  }	
+	
+    private static void print(int i) {
+		System.out.println("element: " + i);		
 	}
 
-    static void threeDimensionalArrays() {
+	static void threeDimensionalArrays() {
 	   System.out.println("\nInside threeDimensionalArrays ...");
 	   int[][][] unitsSold = new int[][][]{ 
 										   { // New York
@@ -111,16 +157,139 @@ package com.semanticsquare.basics;
 	static void varargsOverload(boolean b, int... list){
 	   System.out.println("\nInside varargsOverload with varargs ...");
 	   System.out.println("list.length: " + list.length);
+	   int sum = 0;
+	   for (int i : list) {
+		   sum += i;
+	   }
+	   
+	   System.out.println("sum: " + sum);
 	}	
 	
+	static void preAndPost() {
+		System.out.println("\nInside preAndPost ...");
+		
+		int x = 5;
+		
+		//--x;		
+		//System.out.println("x: " + x);	
+
+		//int y = x--;		
+		//System.out.println("y: " + y + ", x: " +  x);
+		
+		int index = 0;
+		int[] array = new int[3];
+		array[index++] = 10;		
+		array[index++] = 20;		
+		array[index++] = 30;
+		
+		System.out.println(index);
+	}
+	
+	
+	static void compoundArithmeticAssignment() {
+		int x = 100;
+		
+		System.out.println("x += 5: " + (x += 5));
+		System.out.println("x -= 5: " + (x -= 5));
+		System.out.println("x *= 5: " + (x *= 5));
+		System.out.println("x /= 5: " + (x /= 5));
+		System.out.println("x %= 5: " + (x %= 5));
+		
+		// Invalid
+		System.out.println("x =+ 5: " + (x =+ 5)); // Unary plus ~ x = +5
+		System.out.println("x =- 5: " + (x =- 5)); // Unary minus ~ x = -5
+		/*System.out.println("x =* 5: " + (x =* 5));
+		System.out.println("x =/ 5: " + (x =/ 5));
+		System.out.println("x =% 5: " + (x =% 5));*/
+	}
+	
+	static void isOddOrEven(int num) {
+		System.out.println(num % 2);
+	}
+	
 	static void charTypePromotion() {
-	  System.out.println("\nInside charTypePromotion ...");
-	  char char1 = 50; // Will be assigned corresponding UTF16 value 2
-	  System.out.println("char1: " + char1); 
-	  System.out.println("(73 - char1): " + (73 - char1)); // char1 gets promoted to int, i.e., decimal equivalent 50 in UTF16 is used	  
-	  System.out.println("(char1 - '3'): " + (char1 - '3')); // char1 & '3' are promoted to ints	
-	  System.out.println("('a' + 'b'): " + ('a' +'b')); // 'a' & 'b' are promoted to ints and the respective equivalents 97 & 98 are added
-    }
+		  System.out.println("\nInside charTypePromotion ...");
+		  char char1 = 50; // Will be assigned corresponding UTF16 value 2
+		  System.out.println("char1: " + char1); 
+		  System.out.println("(73 - char1): " + (73 - char1)); // char1 gets promoted to int, i.e., decimal equivalent 50 in UTF16 is used	  
+		  System.out.println("(char1 - '3'): " + (char1 - '3')); // char1 & '3' are promoted to ints	
+		  System.out.println("('a' + 'b'): " + ('a' +'b')); // 'a' & 'b' are promoted to ints and the respective equivalents 97 & 98 are added
+	}
+	
+	// Comparison or Relational operators
+		static void comparisonOperators() {
+		    System.out.println("\nInside comparisonOperators ...");
+			int age = 20;
+		    /*if (age > 21) {
+				System.out.println("Graduate student");
+			}*/
+			System.out.println("age > 21: " + (age > 21));
+			System.out.println("age >= 21: " + (age >= 21));  
+			System.out.println("age < 21: " + (age < 21));
+			System.out.println("age <= 21: " + (age <= 21));	  
+			System.out.println("age == 21: " + (age == 21)); // equal to (equality operator)
+			System.out.println("age != 21: " + (age != 21)); // not equal to (equality operator)
+			
+			boolean isInternational = true;
+			//System.out.println("isInternational <= true: " + (isInternational <= true));  
+			System.out.println("isInternational == true: " + (isInternational == true)); 
+			System.out.println("isInternational != true: " + (isInternational != true));
+			
+			/*Student s1 = new Student(1000, "Dheeru");
+			Student s2 = new Student(1000, "Dheeru");
+			System.out.println("s1 == s2: " + (s1 == s2)); // See object class
+			System.out.println("s1 != s2: " + (s1 != s2));
+			
+			update(s1, "John");*/
+		}		
+		
+		/*static boolean update(Student s, String name) {
+		    if (s == null) {
+			   return false;
+			}
+			
+			s.name = name;		
+			return true;
+		}*/
+	
+	static void logicalOperators() {
+		System.out.println("\nInside logicalOperators ...");
+		int age = 37;
+	    int salary = 85000;
+	    boolean hasBadCredit = false;
+		
+		// 1. Core (AND, OR, NOT & Operator Chaining)
+	    
+	    if (age > 35 && salary > 90000 || !hasBadCredit) {
+	    	System.out.println("Loan approved!");
+	    } else {
+			System.out.println("Loan not approved!");
+		}
+		
+		// 2. 
+		//    (a) Left-associative ~ Order of grouping
+		//    (b) Associativity (a && b) && c = a && (b && c)
+		//    Applies to both && and ||
+		
+		
+		// 3. 
+		//    (a) Operator precedence of Logical Operators: Helps with ONLY grouping operations. Not order of execution. (! > && > ||)
+		
+		          // Other Examples: A && B || C && D = (A && B) || (C && D)
+				  //                 A && B && C || D = ((A && B) && C) || D
+		
+		
+		//    (b) Operator Precedence across logical, comparison and arithmetic
+		//          ! > arithmetic > comparison > &&, ||
+		//    See resources section for complete precedence rules
+		
+		// ALWAYS USE PARENTHESIS for READABILITY. Not everyone is aware of precedence rules
+		
+		// 4. Use && to avoid NullPointerException
+		//Student s1 = new Student(1001, "Raj");
+		//update(s1, "Dheeru");
+	}
+	
 	
 	static void bitwiseOperators() {
 	    System.out.println("\nInside bitwiseOperators ...");
@@ -142,10 +311,41 @@ package com.semanticsquare.basics;
 		//double d2 = 5.15;
 		//System.out.println("d1 | d2: " + (d1 | d2));
     }
+	
+	static boolean ifStatement() {
+		boolean approved = false;
+		
+		int age = 27;
+		int salary = 60000;
+		boolean hasBadCredit = false;
+		
+		if (age >= 25 && age <= 35 && salary >= 50000) {		
+			approved = true;            			
+			System.out.println("age >= 25 && age <= 35 && salary >= 50000");
+		} else if (age > 35 && age <= 45 && salary >= 70000) {
+			approved = true;			
+			System.out.println("age > 35 && age <= 45 && salary >= 70000");
+		} else if (age > 45 && age <= 55 && salary >= 90000) {
+			approved = true;			
+			System.out.println("age > 45 && age <= 55 && salary >= 90000");
+		} else {
+		    if (age > 55 && !hasBadCredit) {
+				approved = true;			
+				System.out.println("age > 55 && !hasBadCredit");
+			}
+			System.out.println("else block");
+		}
+		
+				
+		System.out.println("outside if");
+		return approved;
+	}
   
+	
     static void switchExample() {
 	    System.out.println("\nInside switchExample ...");
-	    int month = 3;
+	    
+	    byte month = 3;
 	    switch (month) {
 		    case 1: System.out.println("January");
 	                break;
@@ -155,12 +355,13 @@ package com.semanticsquare.basics;
 	                break;
 		    default: System.out.println("April");
 	    }
-    }
-  
+    }  
+   
+      
     static void labeledBreak() {
 	    System.out.println("\nInside labeledBreak ...");
 	    int num = 0;
-		
+	    
 	    outermost: for (int i = 0; i < 10; i++) {
 	        for (int j = 0; j < 10; j++) {    
 	            if (i == 5 && j == 5) {
@@ -186,7 +387,7 @@ package com.semanticsquare.basics;
 	        }
 	    }
 		
-	    System.out.println("num: " + num); // prints 55		
+	    System.out.println("num: " + num); // prints 95
     }
 	
 	static void stringExamples() {
@@ -219,23 +420,25 @@ package com.semanticsquare.basics;
 		System.out.println("\ns.substring(4): " + s.substring(4));
 		System.out.println("s.substring(4, 9): " + s.substring(4, 9));
 		
-		// Case conversions
+		// Case conversions (Note: String is immutable. So, only a copy is returned)
 		System.out.println("\ns.toUpperCase(): " + s.toUpperCase());
 		System.out.println("s.toLowerCase(): " + s.toLowerCase());
 		
 		System.out.println("\ns.trim(): " + s.trim()); // returns a copy of string after trimming any leading & trailing white spaces
 		
-		// Replace
+		
+		// Replace (e.g., replace comma's with white-space)
 		System.out.println("\ns.replaceAll(\"o\", \"r\"): " + s.replaceAll("o", "r"));
 		
-		// Split
+		// Split (e.g., split a document into words or split a line of text by tab or comma or white space)
 		System.out.println("\ns.split(\"o\"): ");
 		String[] sa = s.split("o");
 		for (String temp : sa) {
 			System.out.println(temp);
 		}
 		
-		System.out.println("\ns.valueOf(1.3): " + s.valueOf(1.3));
+		// Static method (includes overloaded methods)
+		System.out.println("\nString.valueOf(1.3): " + String.valueOf(1.3));
 	}
 	
 	static void stringPool() {
@@ -245,47 +448,99 @@ package com.semanticsquare.basics;
 	  String s3 = "hello!".intern();
 	  String s4 = new String("hello!");	 
 	  String s5 = "lo!";
-
+	  	  
       System.out.println("s1 == s2: " + (s1 == s2));
       System.out.println("s1 == s3: " + (s1 == s3));
       System.out.println("s1 == s4: " + (s1 == s4));
       System.out.println("s1 == s4.intern(): " + (s1 == s4.intern()));
       System.out.println("s1 == \"hel\" + \"lo!\": " + (s1 == "hel" + "lo!"));
-      System.out.println("s1 == \"hel\" + s5: " + (s1 == "hel" + s5));
+      System.out.println("s1 == \"hel\" + s5: " + (s1 == "hel" + s5)); 
+      
     }
+
 		
-	public static void main(String[] args) {	
+	private static void mathDemo() {
+		// Lottery [1, 100]
+		double random = Math.random(); // [0.0, 1.0)
+		int winner = (int) (random * 100) + 1;
+		
+		System.out.println(random);
+		System.out.println(winner);
+		
+		// round ~ rounding floating point to nearest integers
+		long distance1 = Math.round(24.45);
+		int distance2 = Math.round(24.5f);
+		
+		System.out.println(distance1);
+		System.out.println(distance2);
+		
+		// ceil ~ round-up
+		double ceil1 = Math.ceil(24.45);
+		double ceil2 = Math.ceil(25.0);
+		
+		System.out.println(ceil1);
+		System.out.println(ceil2);
+		
+		// floor ~ round down
+		double floor1 = Math.floor(24.45);
+		double floor2 = Math.floor(25.0);
+		
+		System.out.println(floor1);
+		System.out.println(floor2);
+		
+		double max = Math.max(24.45, 24.5);
+		System.out.println(max);
+		
+		double abs = Math.abs(-24.45);
+		System.out.println(abs);
+		
+		double pow = Math.pow(2.0, 4.0);
+		System.out.println(pow);
+		
+		double nan = 0.0/0.0;
+		double sqrt = Math.sqrt(nan);
+		System.out.println(nan);
+		// Intro to Machine learning ~ "deep learning"
+		double idf = Math.log(100/8);
+		System.out.println(idf);
+		
+		//Math math = new Math();
+	}
+	
+	public static void main(String[] args) {
 	  // Language Basics 1
 	  //print();				
 	  //primitives();
+	  //System.out.println(emoji);
+			
+	  //booleanDemo();
 	  //typeCasting();
 	  //arrays();	
       //threeDimensionalArrays();	
-      /*varargsOverload(true, 1, 2, 3);
-      varargsOverload(true, 1, 2, 3, 4, 5, 6, 7, 8);	  
-      varargsOverload(true);*/
-	  //charTypePromotion();
+	 /* varargsOverload(true, 1, 2, 3);
+      varargsOverload(true, 1, 2, 3, 4, 5, 6, 7, 8);
+	  varargsOverload(true);*/
+	  
+	  //preAndPost();
+	  //compoundArithmeticAssignment();
+	  //isOddOrEven(51);
+	  //charTypePromotion();	  
+	  //comparisonOperators();
+      //logicalOperators();		
 	  //bitwiseOperators();
+		
+	  //ifStatement();
+	  
 	  //switchExample();
 	  //labeledBreak();
 	  //labeledContinue();
       // import demo
       // package creation demo
-      //stringExamples();	
-      //stringPool();	 
-	  
-	  String s = "Hello " + " world!";
-	  System.out.println("s: " + s);
-
-	  StringBuffer sb = new StringBuffer(s);
-	  sb.append(" good").append(" morning :)");
-	  System.out.println("sb: " + sb.toString());
-	  System.out.println("sb.length: " + sb.length());
-	  sb.delete(1, 5);
-	  System.out.println("sb: " + sb.toString());
-	  System.out.println("sb.length: " + sb.length());
-	  sb.insert(1, "ey");
-	  System.out.println("sb: " + sb.toString());
-	  System.out.println("sb.length: " + sb.length());
-    }       
-  }
+      //stringExamples();
+	  //stringPool();	
+	  mathDemo();
+	  	
+    }
+		
+		
+}	
